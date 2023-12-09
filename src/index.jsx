da151ed21f7120d1cebb451d8f5c30bab3c3f263
Tionlierite <Tionlierite@gmail.com>
2023-12-09 16:22:19 +0300

    [impr] Added ESLint and Prettier configs

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 // Libraries
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react"
+import ReactDOM from "react-dom/client"
 // Components
-import App from './App.jsx'
+import App from "./App.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 )
