3505ec9abb7e2a895796c3ea3ba181cd9d9d47b8
Tionlierite <Tionlierite@gmail.com>
2023-12-12 16:33:53 +0300

    [feat] Added transition animation on Notification

--- src/widgets/NotificationWidget/NotificationWidget.jsx
+++ src/widgets/NotificationWidget/NotificationWidget.jsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 // Utils
 import { useDispatch, useSelector } from "react-redux"
 // Actions
@@ -16,23 +16,27 @@ export const NotificationWidget = () => {
 	const showNotificationMessage = useSelector(
 		state => state.notification.message
 	)
+	const [isVisible, setIsVisible] = useState("opacity-0")
 
 	useEffect(() => {
-		let timer
 		if (notificationStatus) {
-			timer = setTimeout(() => {
-				dispatch(hideNotification())
-				dispatch(setErrorMessage(""))
+			setIsVisible("opacity-100")
+			const timeoutID = setTimeout(() => {
+				setIsVisible("opacity-0")
+				setTimeout(() => {
+					dispatch(hideNotification())
+					dispatch(setErrorMessage(""))
+				}, 450)
 			}, 3000)
+
+			return () => clearTimeout(timeoutID)
 		}
-
-		return () => clearTimeout(timer)
-	}, [notificationStatus, dispatch])
-
-	if (!notificationStatus) return null
+	}, [notificationStatus])
 
 	return (
-		<div className='fixed left-1/2 top-9 w-full -translate-x-1/2 rounded bg-[#FF6565] px-10 py-4 text-white sm:w-auto'>
+		<div
+			className={`fixed left-1/2 top-9 w-full -translate-x-1/2 rounded bg-[#FF6565] px-10 py-4 text-white transition-opacity ease-in-out sm:w-auto ${isVisible} duration-500`}
+		>
 			{showNotificationMessage}
 		</div>
 	)
